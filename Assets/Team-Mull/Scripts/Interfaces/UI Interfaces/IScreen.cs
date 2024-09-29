7c116cde6f686716c1880f4c07d6fd7fd2d9aba6
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-09-29 22:23:14 +0100

    IScreen interface

---
+++ Assets/Team-Mull/Scripts/Interfaces/UI Interfaces/IScreen.cs
@@ -0,0 +1,13 @@
+
+namespace DevStory.Interfaces.UI
+{
+    /// <summary>
+    /// This interface will hold the screen’s functionality of Screen’s Opening and Closing. 
+    /// </summary>
+    public interface IScreen
+    {
+        void Open();
+
+        void Close();
+    }
+}
---
+++ Assets/Team-Mull/Scripts/UI/Screen.cs
@@ -0,0 +1,15 @@
+using DevStory.Interfaces.UI;
+using UnityEngine;
+
+public class Screen : MonoBehaviour, IScreen
+{
+    public void Close()
+    {
+        
+    }
+
+    public void Open()
+    {
+        
+    }
+}
