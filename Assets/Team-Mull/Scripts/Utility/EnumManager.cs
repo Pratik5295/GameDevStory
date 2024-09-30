6d09eab6481d50031be6046ca2fe9dfeeb343dfc
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2024-09-30 10:42:03 +0100

    Enum Manager and base Screen

---
+++ Assets/Team-Mull/Scripts/Utility/EnumManager.cs
@@ -0,0 +1,15 @@
+
+namespace MetaConstants.EnumManager
+{
+    /// <summary>
+    /// A static class to hold the references to enums being used in the game
+    /// </summary>
+    public static class EnumManager
+    {
+        public enum ScreenState
+        {
+            CLOSED = 0,
+            OPENED = 1
+        }
+    }
+}
