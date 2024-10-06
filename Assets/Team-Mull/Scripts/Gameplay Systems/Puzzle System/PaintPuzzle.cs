07a6c28bed362e5b67d6d944ccb1f04051b7f3b3
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-10-06 12:33:28 +0100

    Context Menu option to populate paint holders

--- Assets/Team-Mull/Scripts/Gameplay Systems/Puzzle System/PaintPuzzle.cs
+++ Assets/Team-Mull/Scripts/Gameplay Systems/Puzzle System/PaintPuzzle.cs
@@ -8,6 +8,31 @@ namespace DevStory.Gameplay.Puzzles
         [SerializeField]
         private List<PaintHolder> paintHolders = new List<PaintHolder>();
 
+        [SerializeField]
+        private Transform content;
+
+        [ContextMenu("Populate Holders")]
+        public void FindPaintHolders()
+        {
+            //Clear if any existing paint holders
+            paintHolders.Clear();
+
+            //Find new holders
+            for(int i = 0; i < content.childCount; i++)
+            {
+                var holder = content.GetChild(i).GetComponent<PaintHolder>();
+                paintHolders.Add(holder);
+            }
+
+            if (paintHolders.Count <= 0)
+            {
+                Debug.LogError("No Paint Holders were found!");
+            }
+            else
+            {
+                Debug.Log("Populating the paint holders via context menu");
+            }
+        }
         public void SubmitPuzzleCheck()
         {
             Debug.Log($"Result is: {CheckPuzzleValidation()}");
