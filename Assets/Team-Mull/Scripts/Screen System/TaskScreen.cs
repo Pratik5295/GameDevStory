b5b2dd45cdfbebc025867a33b416b444f23ac893
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-10-07 22:28:04 +0100

    Task Screen prefab

---
+++ Assets/Team-Mull/Scripts/Screen System/TaskScreen.cs
@@ -0,0 +1,11 @@
+namespace DevStory.UI
+{
+    /// <summary>
+    /// This script will extend the base screen and implement
+    /// the UI for task screen and hold data of relevant tasks
+    /// </summary>
+    public class TaskScreen : Screen
+    {
+
+    }
+}
