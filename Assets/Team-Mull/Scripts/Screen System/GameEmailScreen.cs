23e4a994828cb3afd7cc76da7330aad0d7c73141
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2024-10-07 11:37:24 +0100

    Email screen prefab

---
+++ Assets/Team-Mull/Scripts/Screen System/GameEmailScreen.cs
@@ -0,0 +1,13 @@
+
+namespace DevStory.UI
+{
+    /// <summary>
+    /// This script extends the screen and represents
+    /// all the communications the player would receive
+    /// from the supervisors or te game
+    /// </summary>
+    public class GameEmailScreen : Screen
+    {
+
+    }
+}
