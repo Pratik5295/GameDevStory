8b35998797aefc10db3cf254a50a30637ffed968
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-10-11 11:10:23 +0100

    Event Bus sorting events based on timer

--- Assets/Team-Mull/Scripts/Game Event System/GameEventBus.cs
+++ Assets/Team-Mull/Scripts/Game Event System/GameEventBus.cs
@@ -1,17 +1,22 @@
-using UnityEngine;
+using DevStory.Gameplay.GameTimer;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
 
 namespace DevStory.GameEventSystem
 {
+    [DefaultExecutionOrder(2)]
     public class GameEventBus : MonoBehaviour
     {
         public static GameEventBus Instance = null;
 
         [SerializeField] private List<GameEventSO> gameEvents = new List<GameEventSO>();
 
+        private GameTimerManager gameTimerManager;
         private void Awake()
         {
-            if(Instance == null)
+           if(Instance == null)
             {
                 Instance = this;
             }
@@ -19,6 +24,22 @@ namespace DevStory.GameEventSystem
             {
                 Destroy(gameObject);
             }
+        }
+        private void Start()
+        {
+            gameTimerManager  =  GameTimerManager.Instance;
+            SortEvents(gameEvents);
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        public List<GameEventSO> SortEvents(List<GameEventSO> events)
+        {
+            events.OrderBy(evt => evt.eventData.eventFireTime);
+            return events;
         }
     }
 }
