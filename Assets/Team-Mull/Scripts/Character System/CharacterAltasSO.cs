695403b9710b535c71d3f9019f5cdb531046623c
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2024-10-17 15:31:58 +0100

    Characters Data scriptable object

---
+++ Assets/Team-Mull/Scripts/Character System/CharacterAltasSO.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "CharacterAtlasSO", menuName = "Characters/Create a new Character Atlas")]
+public class CharacterAltasSO : ScriptableObject
+{
+    public List<CharacterDataSO> Characters;
+
+   
+}
---
+++ Assets/Team-Mull/Scripts/Character System/CharacterDataSO.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct CharData
+{
+    public string CharacterName;
+
+    public string CharacterDesignation;
+
+    public Sprite CharacterSprite;
+}
+
+[CreateAssetMenu(fileName = "CharacterSO", menuName = "Characters/Create a New Character")]
+public class CharacterDataSO : ScriptableObject
+{
+    public CharData Data;
+}
+
+public class TestingCharacter : PropertyAttribute
+{
+    public CharacterDataSO characterData;
+
+
+}
