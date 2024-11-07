7bd9bbd677859e66208a693b446c8099e193721d
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2024-11-07 10:23:47 +0000

    Scale with curve script

---
+++ Assets/Team-Mull/Scripts/VFX/ScaleWithCurve.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace DevStory.VFX
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ScaleWithCurve : MonoBehaviour
+    {
+    }
+}
