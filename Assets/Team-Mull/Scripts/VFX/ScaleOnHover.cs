005a79f654e022a412821710c296441eea4983b4
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2024-11-07 11:25:53 +0000

    Scaling with curve complete

---
+++ Assets/Team-Mull/Scripts/VFX/ScaleOnHover.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace DevStory.VFX
+{
+    /// <summary>
+    /// VFX script to 
+    /// </summary>
+
+    public class ScaleOnHover : ScaleWithCurve
+    {
+    }
+}
--- Assets/Team-Mull/Scripts/VFX/ScaleWithCurve.cs
+++ Assets/Team-Mull/Scripts/VFX/ScaleWithCurve.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 
 namespace DevStory.VFX
 {
     /// <summary>
-    /// A generic script
+    /// A generic script for scaling an object with animation curve using DOTween
     /// </summary>
-    public class ScaleWithCurve : MonoBehaviour
+    public class ScaleWithCurve : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
     {
         [SerializeField]
         private float scaleAmount;
 
         private Vector3 targetScale;
+        private Vector3 originalScale;
 
         [SerializeField]
         private AnimationCurve curve;
@@ -19,19 +21,50 @@ namespace DevStory.VFX
         [SerializeField]
         private float duration;
 
+        private Tween scaleTween;
+
+
         private void Start()
         {
+            originalScale = transform.localScale;
+
             targetScale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
-            ScaleTween();
+            
         }
 
         public void ScaleTween()
         {
-            DOTween.To(
+            scaleTween = DOTween.To(
                 () => transform.localScale,
                 x => transform.localScale = x,
                 targetScale, duration).SetEase(curve).SetLoops(-1, LoopType.Yoyo);
         }
 
+        public void ResetScale()
+        {
+            if(scaleTween != null)
+            {
+                scaleTween.Kill();
+            }
+
+            Sequence resetSequence = DOTween.Sequence();
+
+            resetSequence.Append(
+                transform.DOScale(originalScale, duration)  // Tween the scale back to original scale
+            );
+
+            // Kill the sequence after it finishes
+            resetSequence.OnKill(() => transform.localScale = originalScale);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            ScaleTween();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            ResetScale();
+        }
     }
 }
