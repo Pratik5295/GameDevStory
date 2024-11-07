fe3ecef0130a1b58361d6c8d43a892575321a0bc
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2024-11-07 11:04:00 +0000

    Scale UI entity script

--- Assets/Team-Mull/Scripts/VFX/ScaleWithCurve.cs
+++ Assets/Team-Mull/Scripts/VFX/ScaleWithCurve.cs
@@ -4,9 +4,34 @@ using DG.Tweening;
 namespace DevStory.VFX
 {
     /// <summary>
-    /// 
+    /// A generic script
     /// </summary>
     public class ScaleWithCurve : MonoBehaviour
     {
+        [SerializeField]
+        private float scaleAmount;
+
+        private Vector3 targetScale;
+
+        [SerializeField]
+        private AnimationCurve curve;
+
+        [SerializeField]
+        private float duration;
+
+        private void Start()
+        {
+            targetScale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+            ScaleTween();
+        }
+
+        public void ScaleTween()
+        {
+            DOTween.To(
+                () => transform.localScale,
+                x => transform.localScale = x,
+                targetScale, duration).SetEase(curve).SetLoops(-1, LoopType.Yoyo);
+        }
+
     }
 }
